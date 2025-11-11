9f7c987c8dbccfba1acd1154241800d620546b0f
tlsdn97 <tlsdn3606@gmail.com>
2025-11-11 17:12:37 +0900

    Map Name Change

--- A2025_05_12/Source/A2025_05_12/Private/PLevelTP.cpp
+++ A2025_05_12/Source/A2025_05_12/Private/PLevelTP.cpp
@@ -3,22 +3,18 @@
 
 #include "PLevelTP.h"
 
-// Sets default values
 APLevelTP::APLevelTP()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
-// Called when the game starts or when spawned
 void APLevelTP::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void APLevelTP::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
